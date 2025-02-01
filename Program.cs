f082845b515d28e1ae1180ba252b6a83de257d17
phtomazelli <pedrohenritomazelli89@gmail.com>
2025-02-01 15:05:58 -0300

    base do projeto

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using s6exer;
+using System;
+using System.Globalization;
+
+namespace course
+{
+    class Program
+    
+    {
+     static void Main(string[] args) {
+
+            Quartos[] vect = new Quartos[10];
+ 
+
+            Console.Write("Quantos quartos serão alugados? ");
+          int N =  int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+
+                Console.WriteLine();
+                Console.Write($"Aluguel #{i}:");
+
+                Console.Write("Nome: ");
+                string Nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string Email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int Quarto = int.Parse(Console.ReadLine());
+
+                vect [Quarto] = new Quartos(Nome, Email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0;i <= 10; i++)
+            {
+                if (vect[i] != null )
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+
+                }
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Quartos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s6exer
+{
+    internal class Quartos
+    {
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+
+        public Quartos(string nome, string email) {
+        
+        Nome = nome;
+        Email = email;      
+        }
+
+        public override string ToString()
+        {
+
+            return Nome + ", " + Email;
+        }
+
+
+
+
+
+
+    }
+}
